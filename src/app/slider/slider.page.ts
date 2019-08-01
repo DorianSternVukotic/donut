a48a4698b5180b8ce2b8bea48a78df70b2786b9e
DJStern <dorian.sternvukotic@gmail.com>
2019-08-01 23:50:52 +0200

    sliders

---
+++ src/app/slider/slider.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-slider',
+  templateUrl: './slider.page.html',
+  styleUrls: ['./slider.page.scss'],
+})
+export class SliderPage implements OnInit {
+
+  constructor(
+      private router: Router
+  ) { }
+
+  onContinue() {
+    this.router.navigate(['/']);
+  }
+  ngOnInit() {
+  }
+
+}
