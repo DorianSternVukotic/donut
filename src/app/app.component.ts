a6ca022142841918a0259245b173d6693988818c
Sebastian Penafiel <penafiel.sebastian@gmail.com>
2019-08-13 16:19:42 -0500

    moved to ionic3,  styled splash page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,25 @@
-import { Component } from '@angular/core';
-
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-import {Router} from '@angular/router';
+import { Component } from "@angular/core";
+import { Platform } from "ionic-angular";
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { SplashPage } from "../pages/splash/splash";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  templateUrl: "app.html"
 })
-export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar,
-    private router: Router
-  ) {
-    this.initializeApp();
-  }
+export class MyApp {
+  rootPage: any = SplashPage;
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-      this.router.navigateByUrl('slider');
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen
+  ) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
     });
   }
 }
---
+++ src/providers/meal/meal.ts
@@ -0,0 +1,155 @@
+import { Injectable } from "@angular/core";
+import { TCart, TMealGroup } from "../../interfaces";
+
+/*
+  Generated class for the MealProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class MealProvider {
+  private _cart: TCart[] = [];
+  // TODO peer review how to sync these values in components that use them ?
+
+  private availableMeals: TMealGroup[] = [
+    {
+      groupName: "Apetizers",
+      meals: [
+        {
+          id: 0,
+          name: "salad",
+          displayPrice: 20,
+          description: "Mixed salad",
+          image:
+            "https://ichef.bbci.co.uk/news/660/cpsprodpb/3DAD/production/_104898751_gettyimages-844466808.jpg",
+          selected: false,
+          orderedCount: 0
+        },
+        {
+          id: 1,
+          name: "sandwich",
+          displayPrice: 15,
+          description: "A nice sanwich",
+          image:
+            "https://ichef.bbci.co.uk/news/660/cpsprodpb/3DAD/production/_104898751_gettyimages-844466808.jpg",
+          selected: false,
+          orderedCount: 0
+        }
+      ]
+    },
+    {
+      groupName: "MainCourse",
+      meals: [
+        {
+          id: 3,
+          name: "burger",
+          displayPrice: 25,
+          description: "big burger",
+          image:
+            "https://ichef.bbci.co.uk/news/660/cpsprodpb/3DAD/production/_104898751_gettyimages-844466808.jpg",
+          selected: false,
+          orderedCount: 0
+        },
+        {
+          id: 4,
+          name: "casserole",
+          displayPrice: 30,
+          description: "I dont even know what that word means",
+          image:
+            "https://ichef.bbci.co.uk/news/660/cpsprodpb/3DAD/production/_104898751_gettyimages-844466808.jpg",
+          selected: false,
+          orderedCount: 0
+        }
+      ]
+    }
+  ];
+
+  private selectedMeals: TMealGroup[] = [];
+
+  get ids(): TCart[] {
+    return this._cart;
+  }
+  getAvailableMeals() {
+    return this.availableMeals;
+  }
+
+  set replaceCart(cart: TCart[]) {
+    this._cart = cart;
+  }
+
+  addMeal(groupName: string, mealId: number) {
+    this._cart.push({ groupName, mealId, orderedCount: 1 });
+  }
+  removeMeal(mealId: number) {
+    this._cart = this._cart.filter(i => {
+      if (i.mealId === mealId) {
+        i.orderedCount = 0;
+        return false;
+      }
+      return true;
+    });
+  }
+  get cart() {
+    const store = {};
+    this._cart.forEach(({ groupName, mealId, orderedCount }) => {
+      const prev = store[groupName] ? store[groupName] : [];
+      store[groupName] = prev.concat({ mealId, orderedCount });
+    });
+    const cart = Object.keys(store).map(key => {
+      const meals = (store[key] as Array<any>).map(m => {
+        return this.availableMeals
+          .find(g => {
+            return g.groupName === key;
+          })
+          .meals.find(i => {
+            if (i.id === m.mealId) {
+              i.orderedCount = m.orderedCount;
+              return true;
+            }
+            return false;
+          });
+      });
+      return { groupName: key, meals };
+    });
+    return cart;
+  }
+  /**
+   * gets orderedCount by id from form
+   * @param formValues current values of the form
+   */
+  getTotalSelectedMealsPrice(formValues: { key: string }) {
+    let totalPrice = 0;
+    this.cart.forEach(mealGroup => {
+      mealGroup.meals.forEach(meal => {
+        totalPrice += meal.displayPrice * formValues[meal.id];
+      });
+    });
+    return totalPrice;
+  }
+
+  getFullPrice() {
+    let price = 0;
+    const theftRate = 25;
+    let fullPrice: {
+      base: number;
+      VAT: number;
+      total: number;
+    } = {
+      base: 0,
+      VAT: 0,
+      total: 0
+    };
+    this.cart.forEach(mealGroup => {
+      mealGroup.meals.forEach(meal => {
+        price += meal.displayPrice * meal.orderedCount;
+      });
+    });
+    fullPrice.total = price;
+    fullPrice.base = price / (1 + theftRate / 100);
+    fullPrice.VAT = fullPrice.total - fullPrice.base;
+
+    return fullPrice;
+  }
+  constructor() {}
+}
