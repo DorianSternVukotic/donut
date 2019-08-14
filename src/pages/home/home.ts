623056e00eff54e816626d9bf0ed5818791edaa4
Sebastian Penafiel <penafiel.sebastian@gmail.com>
2019-08-14 13:34:58 -0500

    home page, first 2 menus

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { HOME_FIRST, HOME_SECOND } from "../../providers/fakeData";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  constructor(public navCtrl: NavController) {}
 
-  constructor(public navCtrl: NavController) {
-
+  get firstItems() {
+    return HOME_FIRST;
   }
-
+  get secondItems() {
+    return HOME_SECOND;
+  }
 }
---
+++ src/providers/fakeData.ts
@@ -0,0 +1,30 @@
+export const HOME_FIRST = [
+  {
+    title: "Nove slastice dostupne",
+    subtitle: "Lorem ipsum dolor sit amet, consectetur…",
+    imgSrc: "assets/imgs/donuts-hero.jpg"
+  },
+  {
+    title: "Nove slastice dostupne",
+    subtitle: "Lorem ipsum dolor sit amet, consectetur…",
+    imgSrc: "assets/imgs/donuts-hero.jpg"
+  },
+  {
+    title: "Nove slastice dostupne",
+    subtitle: "Lorem ipsum dolor sit amet, consectetur…",
+    imgSrc: "assets/imgs/donuts-hero.jpg"
+  }
+];
+
+export const HOME_SECOND = [
+  {
+    title: "Glavno jelo sa piletinom",
+    buttonText: "Dodano",
+    imgSrc: "assets/imgs/salad.jpg"
+  },
+  {
+    title: "Palacinke sa slaninom",
+    buttonText: "Dodaj",
+    imgSrc: "assets/imgs/pancake.jpg"
+  }
+];
